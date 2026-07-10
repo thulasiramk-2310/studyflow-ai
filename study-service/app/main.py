f230d7a38aed7eec5f23d0387ddc43eacfad311b
thulasiramk-2310 <thulasiramk.git@gmail.com>
2026-07-10 21:11:14 +0530

    feat: scaffold FastAPI Study Service

---
+++ study-service/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+@app.get("/health")
+def health_check():
+    return {"status": "UP", "service": "study-service"}
